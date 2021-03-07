49fee982e2d482f95cc8463c77c9fedac46ecd5d
gaAlmaldonado <gmaldo01@rams.shepherd.edu>
2021-03-07 15:50:38 -0500

    Helpful comment.

--- Helpers/DateHelper.cs
+++ Helpers/DateHelper.cs
@@ -4,6 +4,9 @@ namespace BlazorApp.Helpers
 {
   public class DateHelper
   {
+        /**
+         * Format dates, apparently.
+         */
     public static string formatedDate()
     {
         DateTime date = DateTime.Now;
